f0e5879ba00888a127a3872134a7847080902f45
Dfreex <121714536+Dfreex@users.noreply.github.com>
2026-07-21 19:50:51 +0700

    Update diy-part2.sh

--- config/openwrt_main/diy-part2.sh
+++ config/openwrt_main/diy-part2.sh
@@ -53,3 +53,107 @@ git clone -b main https://github.com/ophub/luci-app-amlogic.git package/luci-app
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Additional customizations ends -------------------------------
+
+# =========================================================================================
+# 1. INJEKSI PAKET SUPER RINGAN KUSTOM KITA (WIFI, SQM, ADGUARD)
+# =========================================================================================
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci=y
+CONFIG_PACKAGE_wireless-tools=y
+CONFIG_PACKAGE_wpad-basic-mbedtls=y
+CONFIG_PACKAGE_iw=y
+CONFIG_PACKAGE_hostapd-common=y
+CONFIG_PACKAGE_luci-app-sqm=y
+CONFIG_PACKAGE_sqm-scripts-tc-cake=y
+CONFIG_PACKAGE_kmod-sched-cake=y
+CONFIG_PACKAGE_luci-app-adguardhome=y
+EOF
+
+# =========================================================================================
+# 2. FULL AUTOMATION SCRIPT (PLUG & PLAY UNTUK STB B860H)
+# =========================================================================================
+# Membuat direktori script auto-setup saat booting pertama
+mkdir -p package/base-files/files/etc/uci-defaults/
+
+cat << "EOF" > package/base-files/files/etc/uci-defaults/99-custom-setup
+#!/bin/sh
+
+# --- 1. KONFIGURASI JARINGAN ---
+# LAN: Menggabungkan Port bawaan STB (eth0) ke Bridge LAN
+uci set network.br_lan=device
+uci set network.br_lan.name='br-lan'
+uci set network.br_lan.type='bridge'
+uci add_list network.br_lan.ports='eth0'
+
+uci set network.lan=interface
+uci set network.lan.device='br-lan'
+uci set network.lan.proto='static'
+uci set network.lan.ipaddr='192.168.1.1'
+uci set network.lan.netmask='255.255.255.0'
+
+# WAN: Menetapkan USB to LAN (eth1) sebagai jalur Internet DHCP
+uci set network.wan=interface
+uci set network.wan.proto='dhcp'
+uci set network.wan.device='eth1'
+
+# Memastikan zona Firewall
+uci set firewall.@zone[0].network='lan'
+uci set firewall.@zone[1].network='wan wan6'
+
+# --- 2. KONFIGURASI WIFI TEST ---
+uci set wireless.@wifi-device[0].disabled='0'
+uci set wireless.@wifi-iface[0].disabled='0'
+uci set wireless.@wifi-iface[0].ssid='OpenWrt-STB'
+uci set wireless.@wifi-iface[0].network='lan'
+
+# --- 3. KONFIGURASI SQM (ANTI-LAG GAMING BANDWIDTH 10MBPS) ---
+uci set sqm.@queue[0].enabled='1'
+uci set sqm.@queue[0].interface='eth1'
+uci set sqm.@queue[0].download='8500'
+uci set sqm.@queue[0].upload='8500'
+uci set sqm.@queue[0].qdisc='cake'
+uci set sqm.@queue[0].script='piece_of_cake.qos'
+uci set sqm.@queue[0].linklayer='none'
+
+# --- 4. KONFIGURASI DNSMASQ (Mencegah Port Bentrok) ---
+uci set dhcp.@dnsmasq[0].port='5353'
+
+# --- 5. KONFIGURASI ADGUARD HOME (AUTO START) ---
+uci set adguardhome.AdGuardHome=adguardhome
+uci set adguardhome.AdGuardHome.enabled='1'
+
+# Simpan semua konfigurasi
+uci commit network
+uci commit firewall
+uci commit wireless
+uci commit sqm
+uci commit dhcp
+uci commit adguardhome
+
+# --- 6. BYPASS WIZARD ADGUARD HOME & AUTO FILTER ---
+mkdir -p /etc/adguardhome
+cat << "YAMLEOF" > /etc/adguardhome.yaml
+bind_host: 0.0.0.0
+bind_port: 3000
+dns:
+  bind_hosts:
+  - 0.0.0.0
+  port: 53
+  bootstrap_dns:
+  - 1.1.1.1
+  - 8.8.8.8
+  upstream_dns:
+  - 1.1.1.1
+  - 8.8.8.8
+filters:
+  - enabled: true
+    url: https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt
+    name: AdGuard DNS filter
+    id: 1
+YAMLEOF
+
+exit 0
+EOF
+
+# Berikan izin eksekusi
+chmod +x package/base-files/files/etc/uci-defaults/99-custom-setup
